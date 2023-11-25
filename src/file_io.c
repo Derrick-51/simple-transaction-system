16a80d31568dec6593c4bc2b0b74375343be05fb
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2023-11-25 05:47:38 -0500

    Create find functions

--- src/file_io.c
+++ src/file_io.c
@@ -2,3 +2,34 @@
 
 #include <stdio.h>
 
+/*
+SCHEMA:
+-------------------------------------------------
+account , ID , branch , name , operation , amount
+-------------------------------------------------
+*/
+
+char* file_findIdLines(char* data, char* searchTarget)
+{
+
+}
+
+char* file_findAccountLines(char* data, char* searchTarget)
+{
+
+}
+
+char* file_findBranchLines(char* data, char* searchTarget)
+{
+
+}
+
+char* file_findNameLines(char* data, char* searchTarget)
+{
+
+}
+
+char* file_findOperationLines(char* data, char* searchTarget)
+{
+
+}
