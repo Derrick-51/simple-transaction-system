7424ae091cc7bf012a363ae97c06d0ec94247db3
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2023-11-27 05:13:25 -0500

    Implement File Import Functions

--- src/file_io.c
+++ src/file_io.c
@@ -1,4 +1,5 @@
 #include "file_io.h"
+#include "linked-list.h"
 
 #include <stdio.h>
 
@@ -9,27 +10,79 @@ account , ID , branch , name , operation , amount
 -------------------------------------------------
 */
 
-char* file_findIdLines(char* data, char* searchTarget)
+#define LINE_BUFFER_SIZE 256
+
+
+int file_lineToString(FILE* file, char* destination)
+{
+    int nextChar = '\0';
+    for(int lineIdx = 0; lineIdx < LINE_BUFFER_SIZE - 1; ++lineIdx)
+    {
+        nextChar = fgetc(file);
+
+        if(nextChar == '\n')
+        {
+            destination[lineIdx] = '\0';
+            return 0;
+        }
+        if(nextChar == EOF)
+        {
+            return EOF;
+        }
+        
+        destination[lineIdx] = (char)nextChar;
+    }
+}
+
+LinkedListPtr file_fileToList(char* filePath)
+{
+    if(!filePath)
+    {
+        printf("Null file path\n");
+    }
+
+    FILE* file = fopen(filePath, 'r');
+
+    if(!file)
+    {
+        printf("Failed to open file\n");
+    }
+
+    LinkedListPtr list = ll_createList(LINE_BUFFER_SIZE);
+    char nextLine[LINE_BUFFER_SIZE];
+    
+    while(1)
+    {
+        if(file_lineToString(file, nextLine) == EOF)
+        {
+            break;
+        }
+
+        ll_insertAfter(list, nextLine);
+    }
+}
+
+char* file_filterLines(LinkedListPtr rowList, char* columnName, char* target)
 {
 
 }
 
-char* file_findAccountLines(char* data, char* searchTarget)
+char* file_findAccountLines(LinkedListPtr list, char* searchTarget)
 {
 
 }
 
-char* file_findBranchLines(char* data, char* searchTarget)
+char* file_findBranchLines(LinkedListPtr list, char* searchTarget)
 {
 
 }
 
-char* file_findNameLines(char* data, char* searchTarget)
+char* file_findNameLines(LinkedListPtr list, char* searchTarget)
 {
 
 }
 
-char* file_findOperationLines(char* data, char* searchTarget)
+char* file_findOperationLines(LinkedListPtr list, char* searchTarget)
 {
 
 }
